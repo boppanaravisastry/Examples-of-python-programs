4979b21ce12bc115de92fb9cb73b31d865735d02
boppanaravisastry <boppanaravisastry@gmail.com>
2019-04-17 17:55:21 +0530

    Unit Conversion

---
+++ feet-inches.py
@@ -0,0 +1,7 @@
+#Unit conversion
+#Convert units from feet to inches.
+feet = float(input('# of feet?'))
+print (feet, "feet is:\t", feet*12.0, "inches")
+#Convert units from inches to feet.
+inches = float(input('# of inches?'))
+print (inches, "inches is:\t", inches/12.0, "feet")
